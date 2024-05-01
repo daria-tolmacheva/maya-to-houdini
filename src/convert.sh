badcaddace4332d3042d65c787e1057f9aa3dcde
dtolmacheva <daritolm@gmail.com>
2024-05-01 15:27:01 +0100

    Add basic version of the script converting textures

---
+++ src/convert.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "Converting textures"
+
+# for d in ./*
+# do
+#   for file in $d/*.png
+#   do
+#   echo "iconvert $file ${file%.*}.rat"
+#   iconvert $file "${file%.*}.rat"
+#   done
+# done
+
+
+# Very basic version
+# just goes though current directory and converts all
+# pngs to rat and leaves them there
+
+directory="./"
+pattern="*.png"
+
+for file in "$directory"/$pattern;
+do
+  echo "iconvert $file ${file%.*}.rat"
+  iconvert $file "${file%.*}.rat"
+done
