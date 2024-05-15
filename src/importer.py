06c848652395a37f61d87b82f1ac7b998bfc3408
dtolmacheva <daritolm@gmail.com>
2024-05-15 20:18:41 +0100

    Assign textures when importing geometry to Houdini + tests

--- src/importer.py
+++ src/importer.py
@@ -5,61 +5,103 @@ import hou
 import sys
 from pathlib import Path
 
-def setupObjScene(assets_dir : str) :
-  # check if geometry dir exists, if yes - assign geometry_dir
-  assets_path = Path(assets_dir)
-  if Path(assets_path / 'Geometry').is_dir() == False :
-    print(f"{assets_path}/Geometry not found")
-    return
-  geometry_dir = Path(assets_path / 'Geometry')
-  # check if textures dir exists, if yes - assign textures_dir
-  # if Path(assets_path / 'Textures').is_dir() == False :
-  #   return
-  # textures_dir = Path(assets_path / 'Textures')
+class HoudiniAlembicsImporter() :
 
-  for dir_object in geometry_dir.iterdir() :
-    if dir_object.is_dir() :
-      # create geo node
-      geo_node = hou.node("/obj").createNode("geo", node_name=dir_object.name+"_Geo")
-      # crete merge node inside
-      main_merge = hou.node("/obj/" + geo_node.name()).createNode("merge", node_name=dir_object.name+"_merge")
-      # create null node inside
-      main_null = hou.node("/obj/" + geo_node.name()).createNode("null", node_name=dir_object.name.upper()+"_FINAL")
-      main_null.setInput(0, main_merge)
-      # go inside the path and call createNodeTree(dir_object)
-      createNodeTree(dir_object, geo_node, main_merge)
-      geo_node.layoutChildren()
+  def setupObjScene(self, assets_dir : str) :
+    # check if geometry dir exists, if yes - assign geometry_dir
+    assets_path = Path(assets_dir)
+    if Path(assets_path / 'Geometry').is_dir() == False :
+      print(f"{assets_path}/Geometry not found")
+      return
+    geometry_dir = Path(assets_path / 'Geometry')
+    # check if textures dir exists, if yes - assign textures_dir
+    if Path(assets_path / 'Textures').is_dir() == False :
+      print(f"Textures folder not found, proceeding without assigning materials.")
+      return
+    self.current_textures_dir = Path(assets_path / 'Textures')
 
-  # layout the nodes
-  hou.node("/obj").layoutChildren()
+    for dir_object in geometry_dir.iterdir() :
+      if dir_object.is_dir() :
+        # create geo node
+        geo_node = hou.node("/obj").createNode("geo", node_name=dir_object.name+"_Geo")
+        # crete merge node inside
+        main_merge = geo_node.createNode("merge", node_name=dir_object.name+"_merge")
+        # create null node inside
+        main_null = geo_node.createNode("null", node_name=dir_object.name.upper()+"_FINAL")
+        main_null.setInput(0, main_merge)
+        # check for specific textures folder
+        self.current_textures_dir = Path(self.current_textures_dir / dir_object.name)
+        # create matnet
+        matnet_node = geo_node.createNode("matnet", node_name=dir_object.name+"_matnet")
+        self.current_matnet = matnet_node
+        # go inside the path and call createNodeTree(dir_object)
+        self.createNodeTree(dir_object, geo_node, main_merge)
+        geo_node.layoutChildren()
+        self.current_matnet.layoutChildren()
 
-def createNodeTree(parent_dir : Path, parent_node : hou.Node, child_merge : hou.Node) :
-  # create node tree
-  for dir_object in parent_dir.iterdir() :
-    if dir_object.is_file() :
-      # create import node
-      abc_import_node = parent_node.createNode("alembic", node_name=dir_object.stem)
-      # set up import file
-      abc_import_node.parm("fileName").set(str(dir_object.resolve()))
-      # set up merge input
-      child_merge.setNextInput(abc_import_node)
-    elif dir_object.is_dir() :
-      # create merge node 
-      new_merge = parent_node.createNode("merge", node_name=dir_object.name+"_merge")
-      # go inside the path and call createNodeTree(dir_object)
-      createNodeTree(dir_object, parent_node, new_merge)
-      # set up merge input
-      child_merge.setNextInput(new_merge)
-    
+    # layout the nodes
+    hou.node("/obj").layoutChildren()
 
+  def createNodeTree(self, parent_dir : Path, parent_node : hou.Node, child_merge : hou.Node) :
+    # create node tree
+    for dir_object in parent_dir.iterdir() :
+      if dir_object.is_file() :
+        # create import node
+        abc_import_node = parent_node.createNode("alembic", node_name=dir_object.stem)
+        # set up import file
+        abc_import_node.parm("fileName").set(str(dir_object.resolve()))
+        # create material in matnet
+        material_node = self.createMaterial(dir_object, parent_node)
+        material_node.setInput(0, abc_import_node)
+        # set up merge input
+        child_merge.setNextInput(material_node)
+      elif dir_object.is_dir() :
+        # create merge node 
+        new_merge = parent_node.createNode("merge", node_name=dir_object.name+"_merge")
+        # go inside the path and call createNodeTree(dir_object)
+        self.createNodeTree(dir_object, parent_node, new_merge)
+        # set up merge input
+        child_merge.setNextInput(new_merge)
 
-def importToFile(scene_filename : str, assets_dir : str) :
-  hou.hipFile.load(scene_filename)
-  setupObjScene(assets_dir)
-  hou.hipFile.setSaveMode(hou.saveMode.Text)
-  hou.hipFile.save(scene_filename)
+  def createMaterial(self, dir_object : Path, parent_node : hou.Node) :
+    # create material
+    material = self.current_matnet.createNode("principledshader::2.0", node_name=dir_object.stem+"_shader")
+    material.parm("basecolorr").set(1)
+    material.parm("basecolorg").set(1)
+    material.parm("basecolorb").set(1)
+    material.parm("rough").set(1)
+    material.parm("reflect").set(0.095)
+    # assign textures
+    # -> Base Color
+    texture_file_name = dir_object.stem + "_BaseColor.rat"
+    if Path(self.current_textures_dir / texture_file_name).is_file() :
+      material.parm("basecolor_useTexture").set(True)
+      material.parm("basecolor_texture").set(str(Path(self.current_textures_dir / texture_file_name)))
+      material.parm("basecolor_useTextureAlpha").set(True)
+    # -> Roughness
+    texture_file_name = dir_object.stem + "_Roughness.rat"
+    if Path(self.current_textures_dir / texture_file_name).is_file() :
+      material.parm("rough_useTexture").set(True)
+      material.parm("rough_texture").set(str(Path(self.current_textures_dir / texture_file_name)))
+    # -> Metallic
+    texture_file_name = dir_object.stem + "_Metallic.rat"
+    if Path(self.current_textures_dir / texture_file_name).is_file() :
+      material.parm("metallic_useTexture").set(True)
+      material.parm("metallic_texture").set(str(Path(self.current_textures_dir / texture_file_name)))
+    # Create material to assign to geo
+    material_node = parent_node.createNode("material", node_name=dir_object.stem+"_material")
+    material_node.parm("shop_materialpath1").set(material.path())
+    return material_node
+
+  def importToFile(self, scene_filename : str, assets_dir : str) :
+    hou.hipFile.load(scene_filename)
+    self.setupObjScene(assets_dir)
+    hou.hipFile.setSaveMode(hou.saveMode.Text)
+    hou.hipFile.save(scene_filename)
+
 
 if __name__ == "__main__":
   scene_filename = sys.argv[1]
   assets_dir = sys.argv[2]
-  importToFile(scene_filename, assets_dir)
+  importer = HoudiniAlembicsImporter()
+  importer.importToFile(scene_filename, assets_dir)
---
+++ src/tests/test_importer.py
@@ -0,0 +1,102 @@
+import hou
+import unittest
+from os.path import exists
+from os import remove
+from pathlib import Path
+
+from src.houdiniSceneSetUp import createNewFile
+from src.importer import HoudiniAlembicsImporter
+
+class TestHoudiniImporter(unittest.TestCase) :
+
+  @classmethod
+  def setUpClass(self) :
+    self.test_file = "./TestFile.hip"
+    self.assets_dir = "./src/tests/Importer_Test_Assets"
+
+  @classmethod
+  def tearDown(self) :
+    if exists(self.test_file): remove(self.test_file)
+
+  @classmethod
+  def sceneSetUp(self) :
+    createNewFile(self.test_file)
+    hou.hipFile.load(self.test_file)
+    # import animation
+    importer = HoudiniAlembicsImporter()
+    importer.importToFile(self.test_file, self.assets_dir)
+
+   # Test node tree structure correct
+  def test_treeStructure(self) :
+    createNewFile(self.test_file)
+    hou.hipFile.load(self.test_file)
+
+    # check things dont exist
+    obj_nodes = hou.node("/obj").children()
+    self.assertEqual(len(obj_nodes), 0)
+
+    # import animation
+    importer = HoudiniAlembicsImporter()
+    importer.importToFile(self.test_file, self.assets_dir)
+
+    # check things exist
+    # obj level
+    obj_nodes = hou.node("/obj").children()
+    self.assertEqual(len(obj_nodes), 1)
+    self.assertEqual(obj_nodes[0].name(), "Final_Kate_v1_Geo")
+
+    # inside geo node
+    geo_children = obj_nodes[0].children()
+    self.assertEqual(len(geo_children), 27)
+    # only test one subtree structure
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Tshirt_material").input(0), hou.node("/obj/Final_Kate_v1_Geo/Final_Tshirt"))
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_UnderWear_material").input(0), hou.node("/obj/Final_Kate_v1_Geo/Final_UnderWear"))
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Clothes_grp_merge").input(0), hou.node("/obj/Final_Kate_v1_Geo/Final_UnderWear_material"))
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Clothes_grp_merge").input(1), hou.node("/obj/Final_Kate_v1_Geo/Final_Tshirt_material"))
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_merge").input(0), hou.node("/obj/Final_Kate_v1_Geo/Final_Clothes_grp_merge"))
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/FINAL_KATE_V1_FINAL").input(0), hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_merge"))
+
+  # Test alembics import
+  def test_importAlembics(self) :
+    self.sceneSetUp()
+
+    # check alembics imported as expected
+    imported_file_name = hou.node("/obj/Final_Kate_v1_Geo/Final_Tshirt").parm("fileName").eval()
+    self.assertEqual(Path(imported_file_name).name, "Final_Tshirt.abc")
+    imported_file_name = hou.node("/obj/Final_Kate_v1_Geo/Final_Eyelash").parm("fileName").eval()
+    self.assertEqual(Path(imported_file_name).name, "Final_Eyelash.abc")
+
+  # Test material creation
+  def test_createMaterial(self) :
+    self.sceneSetUp()
+
+    # check things exist
+    self.assertTrue(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet"))
+    self.assertTrue(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader"))
+    # check material parameters
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("basecolorr").eval(), 1)
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("basecolorg").eval(), 1)
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("basecolorb").eval(), 1)
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("rough").eval(), 1)
+    self.assertEqual(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("reflect").eval(), 0.095)
+    
+  # Test assigning textures
+  def test_createMaterial(self) :
+    self.sceneSetUp()
+
+    # check textures
+    # -> Base Color
+    self.assertTrue(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("basecolor_useTexture").eval())
+    texture_name = hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("basecolor_texture").eval()
+    self.assertEqual(Path(texture_name).name, "Final_Body_BaseColor.rat")
+    # -> Metallic
+    self.assertTrue(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("metallic_useTexture").eval())
+    texture_name = hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("metallic_texture").eval()
+    self.assertEqual(Path(texture_name).name, "Final_Body_Metallic.rat")
+    # -> Roughness
+    self.assertTrue(hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("rough_useTexture").eval())
+    texture_name = hou.node("/obj/Final_Kate_v1_Geo/Final_Kate_v1_matnet/Final_Body_shader").parm("rough_texture").eval()
+    self.assertEqual(Path(texture_name).name, "Final_Body_Roughness.rat")
+
+if __name__ == '__main__':
+    unittest.main()
